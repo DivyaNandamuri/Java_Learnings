22350eb77c1034764a95be3a2d6765256b0d8415
Divya <dxn48340@ucmo.edu>
2024-10-03 18:43:32 -0400

    calculate 2 numbers

--- src/main/java/Intro.java
+++ src/main/java/Intro.java
@@ -1,8 +1,27 @@
 package main.java;
+//adding two numbers
+
+class Calculate{
+    public int add1(int a , int b){
+        return a+b;
+    }
+}
 
 public class Intro {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int num1 = 4;
+        int num2 = 5;
+        //Calling add from other class Calculate
+        Calculate cal = new Calculate();
+        int sum1 = cal.add1(num1,num2);
+        //Calling add method from same class using static keyword
+        int sum2 = add(num1,num2);
+        System.out.println("sum1 :" + sum1);
+        System.out.println("sum2 :" + sum2);
+    }
+
+    public static int add(int a , int b){
+        return a+b;
     }
 
 }
