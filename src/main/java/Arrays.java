4cbc26ef4c049dd6875fe6afa5de7b3fd64936a7
Divya <dxn48340@ucmo.edu>
2024-10-04 14:21:34 -0400

    multi-dimensional

--- src/main/java/Arrays.java
+++ src/main/java/Arrays.java
@@ -1,5 +1,7 @@
 package main.java;
 
+import java.util.Random;
+
 //initialising array and printing it
 public class Arrays {
 
@@ -10,6 +12,24 @@ public class Arrays {
             num[i]=count;
             count++;
             System.out.println(num[i]);
+        }
+
+        //multi-dimensional array
+        int num1[][] = new int[2][3];
+        for (int i=0;i<num1.length;i++){
+            for(int j=0;j<num1[i].length;j++){
+                num1[i][j] = (int)(Math.random()*1000);
+                System.out.print(num1[i][j]+ " ");
+            }
+            System.out.println();
+        }
+
+        //multi dimenisonl other form of looping
+        for(int n[] : num1){
+            for(int m : n){
+                System.out.print(m + " ");
+            }
+            System.out.println();
         }
     }
 
