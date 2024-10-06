28669e40c1408806978e3b2129212b9289d2366a
Divya <dxn48340@ucmo.edu>
2024-10-06 13:02:28 -0400

    encapsulation

--- src/main/java/AccessSpecifiers.java
+++ src/main/java/AccessSpecifiers.java
@@ -2,33 +2,53 @@ package main.java;
 
  public class AccessSpecifiers {
 
-        private String name = "Div";
-        private int age = 11;
-        String gender = "Female";
-        static String firstName = "Sree";
+    private String name = "Div";
+    private int age = 11;
+    String gender = "Female";
+    static String firstName = "Sree";
 
-        public String getName(){
-            return name;
-        }
+     public void setName(String n){
+         name=n;
+     }
 
-        public int getAge(){
-            return age;
-        }
+     public void setAge(int n){
+         age=n;
+     }
 
-        public static void firstName1() {
-            System.out.println("fname :"+firstName);
-        }
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+
+    public static void firstName1() {
+        System.out.println("first name :"+firstName);
+    }
 
  }
 
  class GetDetails {
 
      public static void main(String args[]){
+         //instance class call using object
          AccessSpecifiers obj = new AccessSpecifiers();
+         obj.setName("Satya");
+         obj.setAge(20);
+
+         //access modifers
          System.out.println("name: "+ obj.getName());
          System.out.println("age :" +  obj.getAge());
+
+         //general varibale gender
          System.out.println("gender :" + obj.gender);
-         System.out.println("fname :"+ obj.firstName);
+
+         //static variable call using class name
+         System.out.println("fname :"+ AccessSpecifiers.firstName);
+
+         //static method call using class name
         AccessSpecifiers.firstName1();
      }
  }
